24fece1de0b0918064a034987b46db7c6a136493
sandra <sandrarocio12oc@gmail.com>
2020-05-23 20:26:10 -0500

    initial-structure , json

---
+++ src/core/Components/PageBase.vue
@@ -0,0 +1,19 @@
+<template>
+    <div >
+        <AppHeader />
+        <slot></slot>
+         <AppFooter />
+    </div>
+</template>
+
+<script>
+    import AppHeader from './AppHeader.vue'
+    import AppFooter from './AppFooter.vue'
+    export default {
+        name: 'PageBase',
+        components: {
+            AppHeader , AppFooter 
+        }
+    
+    }
+</script>
---
+++ src/modules/Home/Views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+    <PageBase >
+        <router-view></router-view>
+    </PageBase>
+</template>
+
+<script>
+    import PageBase from '../../../core/Components/PageBase.vue'
+
+    export default {
+        name: "HomeView",
+        components: {PageBase}
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .content-background-forms
+        background-color white
+</style>
